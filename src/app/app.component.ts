5cd89b292470ee9a58aa0c26734631b07f031f29
emre3 <emre3@LAPTOP-L3IAVLVM>
2024-03-04 23:23:41 +0300

    4 mart 2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -6,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ETicaretClient';
+  constructor(private toastr:CustomToastrService){
+    this.toastr.message("as","sa",{
+      messageType:ToastrMessageType.Info,
+      position:ToastrPosition.BottomFullWidth
+    });
+    
+   
+  }
+
 }
---
+++ src/app/services/ui/custom-toastr.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomToastrService {
+
+  constructor(private toastrService:ToastrService) { }
+  message(message:string,title:string,toastrOptions:Partial<ToastrOptions>){
+   this.toastrService[toastrOptions.messageType](message,title,{
+    positionClass:toastrOptions.position
+   });
+   
+  }
+}
+export class ToastrOptions{
+  messageType:ToastrMessageType;
+  position:ToastrPosition;
+}
+export enum ToastrMessageType{
+  Error="error",
+  Success="success",
+  Warning="warning",
+  Info="info"
+}
+export enum ToastrPosition{
+   TopRight="toast-top-right",
+   BottomRight="toast-bottom-right",
+   BottomLeft="toast-bottom-left",
+   TopLeft="toast-top-left",
+   TopFullWidth="toast-top-full-width",
+   BottomFullWidth="toast-bottom-full-width",
+   TopCenter="toast-top-center",
+   BottomCenter="toast-bottom-center"
+}
