cad20e1efe7ddf4cceee0312eae091c5ba76d075
emre3 <emre3@LAPTOP-L3IAVLVM>
2024-03-18 16:54:11 +0300

    18 mart commit

--- src/app/directives/admin/delete.directive.ts
+++ src/app/directives/admin/delete.directive.ts
@@ -1,17 +1,19 @@
 import { Directive, ElementRef, EventEmitter, HostListener, Input, Output, Renderer2 } from '@angular/core';
-import { ProductService } from '../../services/common/models/product.service';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { SpinnerType } from '../../base/base.component';
 import { MatDialog } from '@angular/material/dialog';
 import { DeleteDialogComponent, DeleteState } from '../../dialogs/delete-dialog/delete-dialog.component';
+import { HttpClientService } from '../../services/common/http-client.service';
+import { AlertifyService, MessageType, Position } from '../../services/admin/alertify.service';
+import { HttpErrorResponse } from '@angular/common/http';
 declare var $:any;
 @Directive({
   selector: '[appDelete]'
 })
 export class DeleteDirective {
 
-  constructor(private element:ElementRef,private _renderer:Renderer2,private productService:ProductService,
-    private spinnerService:NgxSpinnerService,public dialog: MatDialog) { 
+  constructor(private element:ElementRef,private _renderer:Renderer2,private httpClientService:HttpClientService,
+    private spinnerService:NgxSpinnerService,public dialog: MatDialog,private alertifyService:AlertifyService) { 
     const img=_renderer.createElement("img");
     img.setAttribute("src","../../../../../assets/delete.png");
     img.setAttribute("style","cursor:pointer;");
@@ -21,20 +23,37 @@ export class DeleteDirective {
   }
 
   @Input() id:string;
+  @Input() controller:string;
   @Output() callBack:EventEmitter<any>=new EventEmitter();
   @HostListener("click")
   async onClick(){
     this.openDialog(async()=>{
       this.spinnerService.show(SpinnerType.BallAtom);
       const td:HTMLTableCellElement=this.element.nativeElement;
-     await this.productService.delete(this.id);
+     await this.httpClientService.delete({
+      controller:this.controller
+     },this.id).subscribe(data=>{
       $(td.parentElement).animate({
         opacity:0,
         left:"+=50",
         height:"toogle"
       },700,()=>{
         this.callBack.emit();
+        this.alertifyService.message("Silme işlemi başarılı",{
+          messageType:MessageType.Success,
+          position:Position.TopRight,
+          dismissOther:true,
+          delay:2
+        })
       })
+     },(errorResponse:HttpErrorResponse)=>{
+      this.alertifyService.message("Silme başarısız",{
+        dismissOther:true,
+        position:Position.TopRight,
+        messageType:MessageType.Error,
+        delay:2
+      })
+     })
     })
   }
 
